686c36a9121dc1189f8ddc3d27e1a308e24447cb
Yesong Choi <to06109@naver.com>
2023-03-11 00:44:28 +0900

    [Init] 초기 환경설정 #18

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
+  },
+  server: {
+    host: 'localhost',
+    port: 3000,
+    open: true,
+  },
+  css: {
+    devSourcemap: true, // 개발자 도구에 소스맵 생성 설정
+  },
+});
