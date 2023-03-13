a1cd407975484e82673b9d09aff344846b46a627
Seung taek Oh <elqkddl@naver.com>
2023-03-14 01:20:26 +0900

    [Feat] 리스트렌더링 추가 #22

--- src/components/Swiper/Swiper.jsx
+++ src/components/Swiper/Swiper.jsx
@@ -1,34 +1,44 @@
+import { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper';
+import { Autoplay, Pagination } from 'swiper';
 
 // Import Swiper Styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-import styles from '@/components/Swiper/Pagination.module.css';
+import Styles from '@/components/Swiper/Pagination.module.css';
 
 export function AllSwiper() {
+  // props 로 이미지 url 받기
+  const slides = ['slide1', 'slide2', 'slide3', 'slide4'];
+
   return (
     <Swiper
-      className={styles.Swiper}
+      className={Styles.Swiper}
+      loop={true}
       pagination={{
         dynamicBullets: true,
         clickable: true,
         bulletElement: 'button',
       }}
-      modules={[Pagination]}
+      speed={1000}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      modules={[Autoplay, Pagination]}
       spaceBetween={20}
       slidesPerView={1}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
+      // onSlideChange={() => console.log('slide change')}
+      // onSwiper={(swiper) => console.log(swiper)}
     >
-      <SwiperSlide className={styles.SwiperSlide}>Test 1</SwiperSlide>
-      <SwiperSlide className={styles.SwiperSlide}>Slide 2</SwiperSlide>
-      <SwiperSlide className={styles.SwiperSlide}>Slide 3</SwiperSlide>
-      <SwiperSlide className={styles.SwiperSlide}>Slide 4</SwiperSlide>
-      <SwiperSlide className={styles.SwiperSlide}>Slide 5</SwiperSlide>
-      <SwiperSlide className={styles.SwiperSlide}>Slide 6</SwiperSlide>
-      <SwiperSlide className={styles.SwiperSlide}>Slide 7</SwiperSlide>
+      {slides.map((slide, index) => {
+        return (
+          <SwiperSlide key={slide} className={Styles.SwiperSlide}>
+            {slides[index]}
+          </SwiperSlide>
+        );
+      })}
     </Swiper>
   );
 }
