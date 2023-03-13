9fd69dc220d073957629cdb85b4f132cd13d4272
seovee <jinseob102@naver.com>
2023-03-13 15:21:26 +0900

    [Feat] Header(+Nav) 컴포넌트 초기 마크업 및 CSS 추가

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Style from './Nav.module.css';
+import { A11yHidden } from '@/components/A11yHidden/A11yHidden';
+
+export default function Nav() {
+  return (
+    <nav className={Style.Nav}>
+      <A11yHidden as="h2" className="srOnly">
+        메인 메뉴
+      </A11yHidden>
+      <ul className={Style.NavList}>
+        <li>
+          <a href="/">Upload</a>
+        </li>
+        <li>
+          <a href="/">Log In</a>
+        </li>
+        <li>
+          <a href="/">Sign Up</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
