d6e195de92de068845e4713cee332fdcd8e6be9a
Yesong Choi <to06109@naver.com>
2023-03-14 16:45:15 +0900

    [Feat] 디자인 문구 컴포넌트 구현 #33

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,14 @@
-import './App.css'
+import './App.css';
+import { DesignParagraph } from '@/components/index';
 
 function App() {
-  return <div className="App"></div>
+  const travelParagraph =
+    '#Snapshot #Golden Gate Bridge | #phtographer #America #i want freedom';
+  return (
+    <div className="App">
+      <DesignParagraph>{travelParagraph}</DesignParagraph>
+    </div>
+  );
 }
 
-export default App
+export default App;
