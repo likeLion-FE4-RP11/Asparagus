d6528b1db748cce2b970812a8557ef26ae4c2db8
Yesong Choi <to06109@naver.com>
2023-03-14 15:40:07 +0900

    [Feat] Top 버튼 gsap 애니메이션 추가 #20

--- src/components/Button/TopButton.jsx
+++ src/components/Button/TopButton.jsx
@@ -1,43 +1,57 @@
 import topButtonIcon from '@/assets/topbutton-icon.svg';
 import Styles from './TopButton.module.css';
 import { getColor } from '@/theme/utils';
-import { useEffect, useState } from 'react';
+import { useLayoutEffect, useEffect, useState, useRef } from 'react';
+import gsap from 'gsap';
 
 export function TopButton() {
   const [showButton, setShowButton] = useState(false);
-
-  const scrollToTop = () => {
-    window.scroll({
-      top: 0,
-      behavior: 'smooth',
-    });
-  };
+  const topButton = useRef();
 
   useEffect(() => {
     const handleTopButton = () => {
-      if (window.scrollY > 800) {
+      if (window.scrollY > 100) {
         setShowButton(true);
       } else {
         setShowButton(false);
       }
     };
-    window.addEventListener('scroll', handleTopButton);
 
+    window.addEventListener('scroll', handleTopButton);
     return () => {
       window.removeEventListener('scroll', handleTopButton);
     };
   }, []);
 
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      if (showButton) {
+        gsap.from(topButton.current, { y: 50, opacity: 0, duration: 0.5 });
+      }
+    }, topButton);
+
+    return () => ctx.revert(); // cleanup
+  }, [showButton]);
+
+  const scrollToTop = () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <>
       {showButton && (
         <button
+          ref={topButton}
           type="button"
           className={Styles.TopButton}
           style={{ backgroundColor: getColor('secondary') }}
           onClick={scrollToTop}
         >
-          <img src={topButtonIcon} alt="위쪽 화살표" />
+          <img src={topButtonIcon} alt="페이지 상단으로 이동" />
           Top
         </button>
       )}
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export * from './Button/Button';
 export * from './Button/TopButton';
+export * from './Button/TopButtonTest';
