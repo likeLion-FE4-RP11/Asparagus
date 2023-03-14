d71a49a7260f5cb5e904d08185505fc2c511e67f
munnarae <skfo789@naver.com>
2023-03-14 13:04:54 +0900

    [Feat] 삭제 버튼 컴포넌트 기능 완료#23

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,11 @@
-import './App.css'
+import { DeleteButton } from '@/components/index';
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <DeleteButton>Delete</DeleteButton>
+    </div>
+  );
 }
 
-export default App
+export default App;
