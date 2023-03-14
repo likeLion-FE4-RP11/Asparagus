6fdedaa6af10e499a43ecfd1145a8d54b51f0a5e
5wintaek <elqkddl@naver.com>
2023-03-14 23:20:30 +0900

    [Feat] Swiper 컴포넌트 생성 (#27)
    * [Feat] Pagination 기능 구현 #22
    
    * [Feat] 스와이퍼 tab접근성 기능 추가 #22
    
    * [Feat] 리스트렌더링 추가 #22
    
    * [Refactor] 사용하지 않는 useState 삭제 #22
    
    * [Refactor] App.css 코드 삭제 #22

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,13 @@
 import Header from '@/components/Header/Header';
+import '@/components/Swiper/Swiper';
+import { AllSwiper } from '@/components/Swiper/Swiper';
 
 function App() {
   return (
     <div>
       <Header />
+      <h1>Test Swiper</h1>
+      <AllSwiper></AllSwiper>
     </div>
   );
 }
