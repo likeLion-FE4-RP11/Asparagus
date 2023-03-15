454fd8d71d6bbf2ee8f8e1003f7ae8f438c53dec
seovee <jinseob102@naver.com>
2023-03-15 22:12:04 +0900

    [Refactor] 카테고리 타이틀 styled-component로 변경 #42

--- src/components/CategoryTitle/CategoryTitle.jsx
+++ src/components/CategoryTitle/CategoryTitle.jsx
@@ -1,9 +1,28 @@
-import Styles from './CategoryTitle.module.css';
+import styled from 'styled-components';
+
+const CategoryTitleContainer = styled.div`
+  background: #f2e9e4;
+  text-align: center;
+  display: block;
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 160px;
+`;
+
+const CategoryContents = styled.h3`
+  color: #4e4c4c;
+  /* display: inline-block; */
+  font-size: 40px;
+  font-weight: 700;
+  line-height: 160px;
+  letter-spacing: 40;
+`;
 
 export function CategoryTitle({ title }) {
   return (
-    <div className={Styles.CategoryTitleContainer}>
-      <h3 className={Styles.CategoryTitle}>{title}</h3>
-    </div>
+    <CategoryTitleContainer>
+      <CategoryContents>{title}</CategoryContents>
+    </CategoryTitleContainer>
   );
 }
