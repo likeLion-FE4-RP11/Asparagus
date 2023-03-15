3cc2ce5fe68fe5c01fb54048ad23ca02567e45ec
최예송 <69625013+to06109@users.noreply.github.com>
2023-03-15 11:26:48 +0900

    [Feat] 이미지 컴포넌트 생성 (#26)
    * [Feat] 이미지 컴포넌트 구현 #19
    
    * [Design] 이미지 컨테이너 border-radius 추가 #19
    
    * [Refactor] className 파스칼케이스로 변경
    
    * [Refactor] module css 이름 변경
    
    * [Feat] 컴포넌트 분리 #19
    
    - 이미지 렌더링 컴포넌트
    - 로딩시 기본 이미지 컴포넌트
    
    * [Refactor] 이미지 절대경로, 사용하지 않는 코드 삭제 #19
    
    * [Style] 사용하지 않는 코드 삭제 #19

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,17 +1,20 @@
 import Header from '@/components/Header/Header';
 import '@/components/Swiper/Swiper';
 import { AllSwiper } from '@/components/Swiper/Swiper';
+import { ImageContainer, BaseImage } from '@/components/index';
 import Footer from '@/components/Footer/Footer';
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <h1>Test Swiper</h1>
-      <AllSwiper></AllSwiper>
-      <Footer />
-    </div>
-  );
+  return(
+  <div>
+    <Header />
+    <h1>Test Swiper</h1>
+    <AllSwiper></AllSwiper>
+    <h2>Image 컴포넌트 테스트</h2>
+    <BaseImage width="430px" height="633px" />
+    <ImageContainer width="430px" height="633px" />
+    <Footer />
+  </div>)
 }
 
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export * from './Button/Button';
+export * from './ImageContainer/ImageContainer';
+export * from './ImageContainer/BaseImage';
 export * from './Swiper/Swiper';
