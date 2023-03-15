1946fc586319081125dc2d1b3fe1a27e2f745649
김진희 <119389578+kyuran6@users.noreply.github.com>
2023-03-15 11:17:35 +0900

    [Feat] 푸터 컴포넌트 생성 (#25)
    * [Feat] 푸터 컴포넌트 생성 #13
    
    * [Refactor] className 변경 & module css 이름 변경
    
    * [Refactor] css 변경

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header/Header';
 import '@/components/Swiper/Swiper';
 import { AllSwiper } from '@/components/Swiper/Swiper';
+import Footer from '@/components/Footer/Footer';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <h1>Test Swiper</h1>
       <AllSwiper></AllSwiper>
+      <Footer />
     </div>
   );
 }
