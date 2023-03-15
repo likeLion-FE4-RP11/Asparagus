4873b76da9bc961b686b00bb5612f2a48a0bfae7
Yesong Choi <to06109@naver.com>
2023-03-15 11:47:22 +0900

    [Style] 세미콜론 삭제 #33

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,28 +2,28 @@ import Header from '@/components/Header/Header';
 import '@/components/Swiper/Swiper';
 import { AllSwiper } from '@/components/Swiper/Swiper';
 import { ImageContainer, BaseImage } from '@/components/index';
-import Footer from '@/components/Footer/Footer';;
-import { TopButton, TopButtonTest } from '@/components/index';;
+import Footer from '@/components/Footer/Footer';
+import { TopButton, TopButtonTest } from '@/components/index';
 import { DesignParagraph } from '@/components/index';
 
 function App() {
   const travelParagraph =
-  '#Snapshot #Golden Gate Bridge | #phtographer #America #i want freedom';
-  
-  return(
-  <div>
-    <Header />
-    <h1>Test Swiper</h1>
-    <AllSwiper></AllSwiper>
-    <h2>Image 컴포넌트 테스트</h2>
-    <BaseImage width="430px" height="633px" />
-    <ImageContainer width="430px" height="633px" />
-    <TopButtonTest />
+    '#Snapshot #Golden Gate Bridge | #phtographer #America #i want freedom';
+
+  return (
+    <div>
+      <Header />
+      <h1>Test Swiper</h1>
+      <AllSwiper></AllSwiper>
+      <h2>Image 컴포넌트 테스트</h2>
+      <BaseImage width="430px" height="633px" />
+      <ImageContainer width="430px" height="633px" />
+      <TopButtonTest />
       <TopButton />
       <DesignParagraph>{travelParagraph}</DesignParagraph>
-    <Footer />
-  </div>)
-
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
