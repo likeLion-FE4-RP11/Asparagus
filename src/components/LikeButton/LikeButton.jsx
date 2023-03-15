82b3afb530e672579890c0ca14bdfc803ec59522
kyran6 <loveheee@gmail.com>
2023-03-15 15:33:41 +0900

    [Refactor] 선언식 변경

--- src/components/LikeButton/LikeButton.jsx
+++ src/components/LikeButton/LikeButton.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import Styles from './LikeButton.module.css';
-import HeartImg from './heart.png';
-import EmptyHeartImg from './empty-heart.png';
+import HeartImg from '@/assets/Heart.png';
+import EmptyHeartImg from '@/assets/empty-heart.png';
 
 
-const LikeButton = ({ item }) => {
+function LikeButton(){
   const [heart, setHeart] = useState(false);
   const [count, setCount] = useState(0);
   
   //좋아요 클릭시
-  function onClick(){
+const handleLikeButton =() =>{
     setHeart((prev) => !prev);
     if(heart){
       setCount(count-1);
@@ -20,15 +20,15 @@ const LikeButton = ({ item }) => {
   };
 
   return (
-    <div className="LikeButtonArea" onClick={onClick}>
+    <button type="button" className="LikeButtonArea" onClick={handleLikeButton}>
       {heart ? (
-        <img src={HeartImg} /> // true (꽉찬하트)>
+        <img src={HeartImg} alt="꽉찬하트"/> // true (꽉찬하트)>
       ) : (
-        <img src={EmptyHeartImg} /> // false (빈 하트)
+        <img src={EmptyHeartImg} alt="빈하트"/> // false (빈 하트)
       )}
       <span className={Styles.LikeButton}>{count}</span>
 
-    </div>
+    </button>
 
   );
 };
