2ee125f45f4f9a5f899841f0bdf353a4582eb5b5
Seung taek Oh <elqkddl@naver.com>
2023-03-16 14:44:16 +0900

    [Feat] 토큰값을 이용하여 색깔 적용 #44

--- src/components/SeeMoreImage/SeeMore.jsx
+++ src/components/SeeMoreImage/SeeMore.jsx
@@ -1,15 +1,16 @@
 import styled from 'styled-components/macro';
+import { getColor } from '@/theme/utils';
 
 const ImageContainer = styled.div`
   margin-top: 20px;
   padding: 20px;
-  background: #eee;
+  background-color: ${(props) => props.bgColor};
   text-align: left;
 `;
 
 export function SeeMoreImg() {
   return (
-    <ImageContainer>
+    <ImageContainer bgColor={getColor('primary')}>
       <h4>테스트</h4>
       <p>테스트1</p>
     </ImageContainer>
