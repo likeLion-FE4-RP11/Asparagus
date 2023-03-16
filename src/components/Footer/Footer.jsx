0bc797a7a4c6ef62e13c5517f1ee486aa14f2654
kyran6 <loveheee@gmail.com>
2023-03-16 17:45:51 +0900

    [Refactor] 푸터 styled-component 파일분리 #13

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,97 +1,28 @@
 import React from 'react';
-import styled from 'styled-components/macro';
 import '@/styles/normarlize.css';
 import '@/styles/reset.css';
 import Logo from '@/assets/Logo.png';
+import * as S from './Footer.styled'
 
-const FooterArea = styled.footer`
-  background: #F2E9E4;
-  padding: 64px 0px;
-  width: 1920px;
-  height: 460px;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Info = styled.section`
-  width: auto;
-  height: 270px;
-  transform:translate(182px,44px);
-  display: absolute;
-`
-
-const FirstHeading = styled.div`
-  font-family: 'Pretendard';
-  font-style: normal;
-  font-weight: 700;
-  font-size: 50px;
-  line-height: 140px;
-  display: flex;
-  align-items: center;
-  color: #4E4C4C;
-`
-
-const FirstParagraph = styled.p`
-  font-family: 'Pretendard';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 21.328px;
-  line-height: 160%;
-  text-align: left;
-  color: #333333;
-  span{
-    display: block;
-  }
-`
-
-const Member = styled.section`
-  width: 733px;
-  height: 270px;  
-  text-align: left;
-  transform:translate(0px,76px);
-  display: absolute;
-`
-const SecondHeading = styled.h2`
-  font-family: 'Pretendard';
-  font-style: normal;
-  font-weight: 700;
-  font-size: 37.897px;
-  line-height: 140%;
-  color: #4E4C4C;
-`
-
-const SecondParagraph = styled.ul`
-  font-family: 'Pretendard';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 21.328px;
-  line-height: 160%;
-  color: #676464;
-`
-
-
-const Footer = () => {
+function Footer(){
   return (
-    <FooterArea>
-        <Info>
-         <FirstHeading>
+    <S.FooterArea>
+        <S.Info>
+         <S.FirstHeading>
            <img src={Logo} alt="아이즈 갤러리 로고" />&nbsp;
-            I&apos;s gallery</FirstHeading>
-          <FirstParagraph>
+            I&apos;s gallery</S.FirstHeading>
+          <S.FirstParagraph>
             <span>Preserve your photos and leave memories.</span>
             <span>I’s gallery is invites you to your exhibition.</span>
             <span>&nbsp;</span>
             <span>COPYRIGHT@2023 | I’S gallery.</span>
-           </FirstParagraph>
-        </Info>
+           </S.FirstParagraph>
+        </S.Info>
       
 
-      <Member>
-        <SecondHeading>Production team</SecondHeading>
-            <SecondParagraph>
+      <S.Member>
+        <S.SecondHeading>Production team</S.SecondHeading>
+            <S.SecondParagraph>
               <li>김진섭&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
               <a href="https://github.com/seovee">https://github.com/seovee</a>
               </li>
@@ -107,9 +38,9 @@ const Footer = () => {
               <li>최예송&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
               <a href="https://github.com/to06109">https://github.com/to06109</a>
               </li>
-            </SecondParagraph>
-      </Member>
-    </FooterArea>
+            </S.SecondParagraph>
+      </S.Member>
+    </S.FooterArea>
         
   )
 }
