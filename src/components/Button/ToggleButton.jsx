902afc25eaf636717cfa7a70243eedaf58ac5ef6
munarae <92195610+munnarae@users.noreply.github.com>
2023-03-16 14:37:08 +0900

    [Refactor] Toggle버튼 스타일 styled-component로 변경 #52 (#54)
    * [Refactor] Delete버튼 스타일 styled-component로 변경
    
    * [Refactor] Delete버튼 임시로직 재변경
    
    * [Feat] 토글 버튼 styled-component로 변경 및 로직변경 #52
    
    * [Refactor] Delete버튼 파일과 충돌나지 않게 내용 통일
    
    * [Refactor] ToggleButton으로 파일명 폴더명 수정

---
+++ src/components/Button/ToggleButton.jsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components/macro';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { getColor } from '@/theme/utils';
+
+const Toggle = styled.div`
+  width: 156px;
+  height: 78px;
+  margin-left: auto;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  border-radius: 50px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+  background-color: ${(props) =>
+    props.isOn === true ? getColor('primary') : getColor('gray/200')};
+  transition: background-color 250ms linear;
+  &:before {
+    content: '';
+    height: 66px;
+    width: 66px;
+    background-color: white;
+    border-radius: 50%;
+    margin: 0 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    left: ${(props) => (props.isOn === true ? '80px' : '0')};
+    transition: left 250ms linear;
+  }
+`;
+
+export function ToggleButton() {
+  const [isToggled, setIsToggled] = useState(false);
+  const handleToggle = () => setIsToggled((prev) => !prev);
+  return (
+    <div>
+      <Button isOn={isToggled} handle={handleToggle} />
+    </div>
+  );
+}
+
+export const Button = ({ isOn, handle }) => {
+  return <Toggle isOn={isOn} onClick={handle} />;
+};
+
+ToggleButton.propTypes = {
+  isToggled: PropTypes.bool,
+  onToggle: PropTypes.func,
+};
+
+Button.propTypes = {
+  isOn: PropTypes.bool,
+  handle: PropTypes.bool,
+};
