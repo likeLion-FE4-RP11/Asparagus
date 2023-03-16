4b6f576f6ee26107c5a29b0bb7bbdc12e184daf6
김진섭 <18073169+seovee@users.noreply.github.com>
2023-03-16 14:08:05 +0900

    [Refactor] 헤더 styled-component로 변경 (#41)
    * [Refactor] 헤더 부분 styled-component 추가변경 #39
    
    * [Refactor] styled-componenets 경로 변경 -> macro #39
    
    * [Refactor] index.js로 경로 변경 #39

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
-import Nav from '@/components/Nav/Nav';
-import Styles from './Header.module.css';
+import Nav from '@/components/index';
 import Logo from '@/assets/Logo.svg';
+import styled from 'styled-components/macro';
+
+const HeaderSection = styled.div`
+  background: #f2e9e4;
+  padding: 0 180px;
+  height: 200px;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export default function Header() {
   return (
-    <header className={Styles.Header}>
-      <div className={Styles.HeaderContainer}>
+    <HeaderSection>
+      <HeaderContainer>
         <h1>
           <a href="/">
             <img src={Logo} alt="아이즈 갤러리 로고" />
           </a>
         </h1>
         <Nav />
-      </div>
-    </header>
+      </HeaderContainer>
+    </HeaderSection>
   );
 }
