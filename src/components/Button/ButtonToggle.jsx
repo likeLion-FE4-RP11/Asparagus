8b8c2a70b5d7b15e10c5a17b94e40dfa2d281044
munnarae <skfo789@naver.com>
2023-03-16 09:29:27 +0900

    [Refactor] Delete버튼 임시로직 재변경 및 설명 추가

--- src/components/Button/ButtonToggle.jsx
+++ src/components/Button/ButtonToggle.jsx
@@ -1,14 +1,51 @@
-import Styles from './ButtonToggle.module.css';
+import styled from 'styled-components/macro';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { getColor } from '@/theme/utils';
 
-export function ButtonToggle({ isToggled, onToggle }) {
+const Toggle = styled.div`
+  width: 156px;
+  height: 78px;
+  margin-left: auto;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  border-radius: 50px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+  background-color: ${(props) =>
+    props.isOn === true ? getColor('primary') : getColor('gray/200')};
+  transition: background-color 300ms linear;
+  &:before {
+    content: '';
+    height: 66px;
+    width: 66px;
+    background-color: white;
+    border-radius: 50%;
+    margin: 0 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    left: ${(props) => (props.isOn === true ? '80px' : '0')};
+    transition: left 300ms linear;
+  }
+`;
+
+export function ButtonToggle() {
+  const [isToggled, setIsToggled] = useState(false);
+  const handleToggle = () => setIsToggled((prev) => !prev);
   return (
-    <label className={Styles.Switch}>
-      <input type="checkbox" checked={isToggled} onChange={onToggle} />
-      <span className={Styles.Slider}></span>
-    </label>
+    <div>
+      <ToggleButton isOn={isToggled} handle={handleToggle} />
+    </div>
   );
 }
+
+export const ToggleButton = ({ isOn, handle }) => {
+  return <Toggle isOn={isOn} onClick={handle} />;
+};
 
 ButtonToggle.propTypes = {
   isToggled: PropTypes.bool,
