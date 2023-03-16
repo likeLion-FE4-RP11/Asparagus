b87e7730b3670bc8c5ac864a0cab47bb252798c1
Seung taek Oh <elqkddl@naver.com>
2023-03-16 15:39:56 +0900

    [Feat] getColor 이용하여 색 가져오기 #44

--- src/components/Button/SeeMoreButton.jsx
+++ src/components/Button/SeeMoreButton.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import { SeeMoreImg } from '@/components/index';
 import styled from 'styled-components/macro';
+import { getColor } from '@/theme/utils';
 
 const MoreButton = styled.button`
   cursor: pointer;
   background: white;
-  color: 4e4c4c;
+  color: ${(props) => {
+    props.color;
+  }};
   padding: 20px 220px;
   border: 20;
   border-radius: 10px;
@@ -23,7 +26,11 @@ export function SeeMoreButton() {
 
   return (
     <div>
-      <MoreButton type="button" onClick={handleSeeMore}>
+      <MoreButton
+        type="button"
+        onClick={handleSeeMore}
+        color={getColor('secondary')}
+      >
         S E E M O R E
       </MoreButton>
       {modal == true ? <SeeMoreImg /> : null}
