d0675d550d0aaf3dcbbb2c3c856e04750e25b96c
Seung taek Oh <elqkddl@naver.com>
2023-03-18 01:42:00 +0900

    [Feat] useRef 사용하여 리렌더링 방지 #58

--- src/components/UploadPage/ImageDescription.jsx
+++ src/components/UploadPage/ImageDescription.jsx
@@ -1,34 +1,54 @@
 import { A11yHidden } from '@/components';
-import { useEffect, useId, useRef, useState } from 'react';
-import * as S from './ImageDescription.styled';
+import { useEffect, useId, useRef } from 'react';
+import styled from 'styled-components/macro';
+import { getColor } from '@/theme/utils';
+
+const TextArea = styled.textarea`
+  background-color: ${(props) => props.bgColor};
+  width: 1563px;
+  height: 500px;
+  font-weight: 400;
+  font-size: 43px;
+  line-height: 160px;
+  padding: 0 0 0 53px;
+  &::placeholder {
+    color: #333333;
+  }
+`;
+
+// const TextInput = styled.TextInput.attr({
+//   placeholderTextColor: 'red',
+//   textarea::
+// });
 
 export function DescriptionInput() {
   const id = useId();
-  const inputRef = useRef(null);
-  const [name, setName] = useState('');
+  const inputRef = useRef('');
 
   const handleInputChange = (e) => {
-    setName(e.target.value);
+    inputRef.current = e.target.value;
   };
 
-  useEffect(() => {
-    console.log('렌더링');
-  }, []);
+  const submit = async () => {
+    // fetch, axios
+    console.log(inputRef.current);
+    // const result = await fetch('POST', '주소');
+  };
+
   return (
     <section>
       <A11yHidden as="label" htmlFor={id}>
         이미지 설명란
       </A11yHidden>
-      <input
-        placeholder="설명을 입력해주세요"
+      <TextArea
+        placeholder="please write image description"
         type="text"
         id={id}
         ref={inputRef}
-        value={name}
         onChange={handleInputChange}
+        bgColor={getColor('gray/50')}
       />
+      <button onClick={submit}>전송</button>
     </section>
   );
 }
-
-// onchange , value
--- src/components/UploadPage/ImageDescription.styled.jsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components/macro';
-
-export const inputed = styled.input`
-  translate: -10px -100%;
-  scale: 0.64;
-`;
