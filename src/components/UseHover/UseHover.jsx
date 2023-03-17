5f6f9636c56472dbb8968ebfaf717c8f385cf6f8
seovee <jinseob102@naver.com>
2023-03-17 13:36:26 +0900

    [Refactor] framer motion 수정 - 1 #32

--- src/components/UseHover/UseHover.jsx
+++ src/components/UseHover/UseHover.jsx
@@ -1,29 +1,37 @@
 import { useCallback, useState, useEffect } from 'react';
-import { ImageContainer } from '@/components/ImageContainer/ImageContainer';
+import { ImageContainer } from '@/components/index';
 import styled from 'styled-components/macro';
 import { motion } from 'framer-motion';
 
 export function UseHover(props) {
   return (
-    <motion.div
-      style={{
-        width: 100,
-        height: 100,
-        margin: 0,
-        background: '#666',
-        borderRadius: 5,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-      {...props}
-    >
-      <p>아래에 글자가 나오나요?</p>
-    </motion.div>
+    <HoverContainer>
+      <motion.div
+        style={{
+          width: 300,
+          height: 300,
+          margin: 0,
+          background: '#666',
+          borderRadius: 5,
+          position: 'absolute',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        {...props}
+      >
+        <HoverScript>아래에 글자가 나오나요?</HoverScript>
+      </motion.div>
+      <ImageContainer />
+    </HoverContainer>
   );
 }
 
 const HoverScript = styled.p`
   font-size: 100px;
   font-weight: 700;
+`;
+
+const HoverContainer = styled.div`
+  position: relative;
 `;
