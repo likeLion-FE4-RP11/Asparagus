6114e461a6755fdc72893413a0057a6c9daf554f
Yesong Choi <to06109@naver.com>
2023-03-20 17:08:02 +0900

    [Feat] Accordion 컴포넌트 기능 완성 #59

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -7,53 +7,62 @@ import '@/styles/normarlize.css';
 export function Accordion() {
   const parentRef = useRef(null);
   const childRef = useRef(null);
-  const[isCollapse, setIsCollapse] = useState(false);
-  
+  const categories = ['Daily', 'Travel', 'Food', 'Hobby'];
+  const [isCollapse, setIsCollapse] = useState(false);
 
   const handleButtonClick = useCallback(
-    event => {
+    (event) => {
       event.stopPropagation();
       if (parentRef.current === null || childRef.current === null) {
         return;
       }
-      if (parentRef.current.clientHeight > 0){
-        parentRef.current.style.height = "0";
+      if (parentRef.current.clientHeight > 0) {
+        parentRef.current.style.height = '0';
       } else {
-        parentRef.current.style.height =
-        `${childRef.current.clientHeight}px`;
+        parentRef.current.style.height = `${childRef.current.clientHeight}px`;
       }
       setIsCollapse(!isCollapse);
     },
     [isCollapse]
   );
 
-  const parentRefHeight = parentRef.current?.style.height ?? "0px";
-  const buttonText = parentRefHeight === "0px" ? <img src={ArrowDown} alt="열기" />: <img src={ArrowUp} alt="닫기" />
+  const parentRefHeight = parentRef.current?.style.height ?? '0px';
+  const buttonText =
+    parentRefHeight === '0px' ? (
+      <img src={ArrowDown} alt="열기" />
+    ) : (
+      <img src={ArrowUp} alt="닫기" />
+    );
 
+  const handleSelect = (e) => {
+    if (e.target.nodeName === 'LI') {
+      console.log(e.target.innerText);
+    }
+  };
 
-  return(
+  return (
     <S.Container>
       <S.Header>
         Category options
-        <S.Button type="button" onClick={handleButtonClick}>{buttonText}</S.Button>
+        <S.Button type="button" onClick={handleButtonClick}>
+          {buttonText}
+        </S.Button>
       </S.Header>
       <S.ContentsWrapper ref={parentRef}>
-        <S.Contents ref={childRef}>
-        <ul>
-            <li>Daily</li>
-            <li>Travel</li>
-            <li>Food</li>
-            <li>Hobby</li>
-            <li>Pet</li>
-            <li>Fashion</li>
-            <li>Movie</li>
-            <li>Excercise</li>
-          </ul>
+        <S.Contents ref={childRef} onClick={handleSelect}>
+          {renderCategory(categories)}
         </S.Contents>
       </S.ContentsWrapper>
     </S.Container>
-
-
-    
   );
 }
+
+const renderCategory = (categories) => {
+  return (
+    <ul>
+      {categories.map((category, index) => (
+        <li key={index}>{category}</li>
+      ))}
+    </ul>
+  );
+};
--- src/components/Accordion/Accordion.styled.jsx
+++ src/components/Accordion/Accordion.styled.jsx
@@ -1,22 +1,22 @@
-import styled from "styled-components/macro";
+import styled from 'styled-components/macro';
 
 export const Container = styled.section`
-background: flex;
-position: relative;
-flex-direction: column;
-justify-content: center;
-border-radius: 4px;
-border: 1px solid silver;
-width: 486.93px;
+  background: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 4px;
+  border: 1px solid silver;
+  width: 486.93px;
 `;
 
 export const Header = styled.section`
-display: flex;
-align-items: center;
-height: 106.24px;
-padding: 27px 35px 27px 35px;
-font-size: 35.41px;
-font-weight: 600;
+  display: flex;
+  align-items: center;
+  height: 106.24px;
+  padding: 27px 35px 27px 35px;
+  font-size: 35.41px;
+  font-weight: 600;
 `;
 
 export const Button = styled.button`
@@ -27,7 +27,8 @@ export const Button = styled.button`
   background-color: transparent;
   position: absolute;
   right: 35px;
-`
+  cursor: pointer;
+`;
 
 export const ContentsWrapper = styled.section`
   height: 0;
@@ -41,36 +42,35 @@ export const Contents = styled.section`
   padding: 4px 8px;
 
   ul {
-  display: flex;
-  flex-direction: column;
-  height: fit-content;
-  width: 100%;
-  padding: 0;
-  margin: 0;
-}
+    display: flex;
+    flex-direction: column;
+    height: fit-content;
+    width: 100%;
+    padding: 0;
+    margin: 0;
+  }
 
-li {
-  display: flex;
-  flex-flow: column nowrap;
-  align-items: flex-start;
-  height: 50px;
-  padding-left: 10px;
-  list-style: none;
-  background-color: #ffffff;
-  margin: 26.56px 0;
-  position: relative;
-}
+  li {
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: flex-start;
+    height: 50px;
+    padding-left: 10px;
+    list-style: none;
+    background-color: #ffffff;
+    margin: 26.56px 0;
+    position: relative;
+    cursor: pointer;
+  }
 
-li::after {
-  content: "";
-  display: block;
-  width: 100%;
-  height: 1px;
-  background-color: #999999;
-  position: absolute;
-  top: 66.56px;
-  left: 0;
-}
+  li::after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 1px;
+    background-color: #999999;
+    position: absolute;
+    top: 66.56px;
+    left: 0;
+  }
 `;
-
-
