9fb8e6d5d1f8502779c7724d68c0ec1ffffcaf88
Seung taek Oh <elqkddl@naver.com>
2023-03-21 17:50:54 +0900

    [Feat] button 컴포넌트 css #92

--- src/components/Button/LoginButton.jsx
+++ src/components/Button/LoginButton.jsx
@@ -1,15 +1,27 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { getColor } from '@/theme/utils';
 
 const ButtonStyle = styled.button`
+  width: 541px;
+  height: 65px;
   user-select: none;
   cursor: pointer;
-  background-color: red;
+  border: 0px;
+  border-radius: 11px;
+
+  color: #000
+  background-color: ${(props) => props.bgColor};
 `;
 
 export function LoginButton({ disabled, children, ...restProps }) {
   return (
-    <ButtonStyle type="button" disabled={disabled} {...restProps}>
+    <ButtonStyle
+      type="button"
+      disabled={disabled}
+      bgColor={getColor('gray/50')}
+      {...restProps}
+    >
       {children}
     </ButtonStyle>
   );
