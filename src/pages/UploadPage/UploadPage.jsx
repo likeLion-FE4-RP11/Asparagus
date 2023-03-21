c57ea50e53d3b3e66bdf1bf7303c5c620c7afe20
Yesong Choi <to06109@naver.com>
2023-03-21 21:18:07 +0900

    [Feat] 업로드 페이지 컴포넌트 배치 #87

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -1,6 +1,57 @@
 import { useDocumentTitle } from '@/hooks/useDocumentTitle';
+import {
+  ImageUploadInput,
+  Accordion,
+  DeleteButton,
+  DescriptionInput,
+  SeeMoreButton,
+} from '@/components';
+
+import { getColor, getFontSize } from '@/theme/utils';
+import { useCallback } from 'react';
+import * as S from './UploadPage.styled';
 
 export default function UploadPage() {
   useDocumentTitle('UploadPage');
-  return <div>업로드 페이지</div>;
+
+  const handelUpload = useCallback(() => {
+    console.log('업로드 버튼 클릭');
+  }, []);
+
+  const deleteButtonStyle = {
+    position: 'absolute',
+    right: '0',
+    marginTop: '2.9375rem',
+  };
+
+  return (
+    <S.FlexContainer>
+      <S.ImageSection>
+        <S.ImageTitle color={getColor('content')} fontSize={getFontSize('4xl')}>
+          My own exhibition
+        </S.ImageTitle>
+        <ImageUploadInput />
+        <DeleteButton style={deleteButtonStyle} />
+        <Accordion style={{ margin: '2.9375rem 0 6.25rem 0' }} />
+      </S.ImageSection>
+
+      <S.DescriptionSection>
+        <S.DescriptionTitle
+          color={getColor('secondary')}
+          fontSize={getFontSize('3xl')}
+        >
+          Enter image description
+        </S.DescriptionTitle>
+        <DescriptionInput />
+        <DeleteButton style={deleteButtonStyle} />
+      </S.DescriptionSection>
+
+      <SeeMoreButton
+        onClick={handelUpload}
+        style={{ marginBottom: '9.9375rem' }}
+      >
+        upload
+      </SeeMoreButton>
+    </S.FlexContainer>
+  );
 }
