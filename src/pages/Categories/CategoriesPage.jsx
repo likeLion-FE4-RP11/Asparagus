68e4b7f89c592007b4a448dd91d19592b9ba6817
문나래 <skfo789@naver.com>
2023-03-22 04:45:54 +0900

    [Feat] 기본 그리드 및 반응형 설정 #88

--- src/pages/Categories/CategoriesPage.jsx
+++ src/pages/Categories/CategoriesPage.jsx
@@ -19,7 +19,22 @@ export default function CategoriesPage() {
   return (
     <Container>
       <ToggleButton />
-      <ImageContainer width={1557} height={769} />
+      <ImageContainer width={'1557'} height={'769'} />
+      <S.FirstContainer>
+        <UseHover width={'894'} height={'525'} />
+        <UseHover width={'632'} height={'525'} />
+        <UseHover width={'626'} height={'525'} />
+        <UseHover width={'900'} height={'525'} />
+      </S.FirstContainer>
+      <DesignParagraph>
+        #Snapshot #Golden Bridge #Photographer #America #I want freedom
+      </DesignParagraph>
+      <S.SecondContainer>
+        <UseHover width={'1097'} height={'633'} />
+        <UseHover width={'430'} height={'633'} />
+        <UseHover width={'739'} height={'416'} />
+        <UseHover width={'789'} height={'416'} />
+      </S.SecondContainer>
     </Container>
   );
 }
--- src/pages/Categories/CategoriesPage.styled.jsx
+++ src/pages/Categories/CategoriesPage.styled.jsx
@@ -1,1 +1,28 @@
 import styled from 'styled-components/macro';
+
+export const FirstContainer = styled.section`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 1.2rem;
+  padding: 1rem;
+  margin-top: 6rem;
+  margin-bottom: 8.75rem;
+  @media screen and (max-width: 940px) {
+    grid-template-columns: 1fr;
+    grid-gap: 1.2rem;
+  }
+`;
+
+export const SecondContainer = styled.section`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 1.2rem;
+  padding: 1rem;
+  margin-top: 8.75rem;
+  margin-bottom: 8.75rem;
+  @media screen and (max-width: 940px) {
+    grid-template-columns: 1fr;
+    grid-gap: 1.2rem;
+`;
