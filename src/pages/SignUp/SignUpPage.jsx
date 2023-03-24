c1fda712e91fdbad7157160af4b346f5e9f5a5be
김진섭 <18073169+seovee@users.noreply.github.com>
2023-03-24 14:39:02 +0900

    [회원가입] 체크박스 컴포넌트 구현, 회원가입 페이지 마크업 완성 (#111)
    * [Feat] 체크박스 컴포넌트 생성 #105
    
    * [Feat] 체크박스 컴포넌트 완성 #105
    
    * [Feat] 회원가입 페이지 완성 #96
    
    * [Refactor] PR요청사항 수정 #96 #105

--- src/pages/SignUp/SignUpPage.jsx
+++ src/pages/SignUp/SignUpPage.jsx
@@ -1,33 +1,94 @@
-import { useDocumentTitle } from '@/hooks/useDocumentTitle';
-import { CategoryTitle, FormInput, ImageContainer } from '@/components/index';
+import { useRef } from 'react';
+import { CheckBox } from '@/components';
 import * as S from './SignUpPase.styled';
+import { useSignUp, useAuthState } from '@/firebase/auth';
+import MainImage from '../../assets/SignUp_main.jpg';
+import { useDocumentTitle } from '@/hooks/useDocumentTitle';
+import {
+  CategoryTitle,
+  SignUpFormInput,
+  ImageContainer,
+} from '@/components/index';
+import { DeprecatedLayoutGroupContext } from 'framer-motion';
+
+const initialFormState = {
+  name: '',
+  email: '',
+  password: '',
+  passwordConfirm: '',
+};
+
 export default function SignUpPage() {
   // 브라우저탭 이름
-  useDocumentTitle('SignUpPage');
+  useDocumentTitle('회원가입 → Likelion 4th');
+
+  const { signUp } = useSignUp();
+  const { isLoading, error, user } = useAuthState();
+  const formStateRef = useRef(initialFormState);
+
+  //
+
+  const SignUpPageReset = () => {
+    console.log('reset');
+  };
+
+  const SignUpSubmit = async (e) => {
+    e.preventDefault();
+
+    const { name, email, password, passwordConfirm } = formStateRef.current;
+
+    await signUp(email, password, name);
+  };
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    formStateRef.current[name] = value;
+  };
+
+  const test = (e) => {
+    console.log(e.target.value);
+  };
 
   return (
-    <S.SignUpContainer>
-      <S.SignUpContent>
-        <S.FormContainer>
-          <S.SingUpTitle>Create an acount</S.SingUpTitle>
-          <FormInput type="text" label="Name" name="이름" />
-          <FormInput type="email" label="Email" name="이메일" />
-          <FormInput type="password" label="Password" name="비밀번호" />
-          <FormInput
-            type="password"
-            label="Password Check"
-            name="비밀번호확인"
-          />
-          <S.FormCheckBox>
-            I agree to the Terms of Service and Privacy Notice
-          </S.FormCheckBox>
-          <S.SignUpButton>Sign me up!</S.SignUpButton>
-        </S.FormContainer>
-      </S.SignUpContent>
-      <S.HalfImageContainer>
-        <S.ImageLogo>I's gallery</S.ImageLogo>
-        {/* 여기에 img 추가해야함 */}
-      </S.HalfImageContainer>
-    </S.SignUpContainer>
+    <>
+      <S.SignUpContainer>
+        <S.SignUpContent>
+          <form onSubmit={SignUpSubmit} onReset={SignUpPageReset}>
+            <S.SingUpTitle>Create an acount</S.SingUpTitle>
+            <SignUpFormInput
+              type="text"
+              label="이름"
+              name="name"
+              onChange={handleChangeInput}
+            />
+            <SignUpFormInput
+              type="email"
+              label="이메일"
+              name="email"
+              onChange={handleChangeInput}
+            />
+            <SignUpFormInput
+              type="password"
+              label="비밀번호"
+              name="password"
+              onChange={handleChangeInput}
+            />
+            <SignUpFormInput
+              type="password"
+              label="비밀번호 확인"
+              name="passwordConfirm"
+              onChange={handleChangeInput}
+            />
+            <CheckBox ref={formStateRef} context="Sign me up!">
+              I agree to the Terms of Service and Privacy Notice
+            </CheckBox>
+          </form>
+        </S.SignUpContent>
+        <S.HalfImageContainer>
+          <S.ImageLogo>I`s gallery</S.ImageLogo>
+          <S.SignUpMainImage src={MainImage} alt="회원가입 메인 이미지" />
+        </S.HalfImageContainer>
+      </S.SignUpContainer>
+    </>
   );
 }
