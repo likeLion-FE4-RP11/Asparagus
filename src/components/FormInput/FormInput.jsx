4721719a204317c21aaccf18b4d869ee1973fecf
5wintaek <elqkddl@naver.com>
2023-03-26 11:51:47 +0900

    [Feat] 로그인 파이어베이스 기능 및 소셜로그인 컴포넌트 (#128)
    * [Feat] FormInput 이메일 창 기능 #85
    
    * [Feat] FormInput 이메일,비밀번호 컴포넌트#85
    
    * [Feat] button 컴포넌트 구현#92
    
    * [Feat] button 컴포넌트 css #92
    
    * [Feat] FormInput styled component 적용#85
    
    * [Feat] Login styled component 재적용 #92
    
    * [Feat] button 컴포넌트 not-allowd 추가 #85
    
    * [Style] FormInput style 수정 #85
    
    * [Feat] 구글 및 페이스북 컴포넌트 구성 버튼재사용 #99
    
    * [Feat] 회원가입 이동 버튼 만들기 #107
    
    * [Feat] 로그인버튼 파이어베이스 연동 #91
    
    * [Feat] 로그인 후 로그아웃 버튼 추가 #91
    
    * [Feat] Google 로그인 성공 #99
    - 1. 자동로그인 해제해야함
    - 2. 파이어베이스에 전달이 안됌
    
    * [Feat] 페이스북 로그인 성공 #99
    
    * [Style] 로그인버튼 isloading 효과 추가 #91

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,3 +1,4 @@
+import { useId, useRef } from 'react';
 import { useId } from 'react';
 import { bool, string } from 'prop-types';
 import { A11yHidden } from '@/components/';
@@ -5,11 +6,12 @@ import * as S from './FormInput.styled';
 
 export function FormInput({ label, type, invisibleLabel, ...restProps }) {
   const id = useId();
+
+
   return (
     <S.FormInput>
       {renderLabel(id, label, invisibleLabel)}
       <S.InputStyle type={type} id={id} {...restProps} />
-      <input type="text" {...restProps} />
     </S.FormInput>
   );
 }
@@ -27,10 +29,10 @@ FormInput.propTypes = {
 
 function renderLabel(id, label, invisibleLabel) {
   return invisibleLabel ? (
-    <A11yHidden as="label" htmlFor={id}>
+    <A11yHidden as="label" htmlFor={id} className="label">
       {label}
     </A11yHidden>
   ) : (
-    <S.LabelStyle htmlFor={id}>{label}</S.LabelStyle>
+    <S.labelStyle htmlFor={id}>{label}</S.labelStyle>
   );
 }
---
+++ src/firebase/auth/GoogleLogin.js
@@ -0,0 +1,3 @@
+// import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+// console.log(provider);
