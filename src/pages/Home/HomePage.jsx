028c85a9a6f765871d8cf3e6cca357b48eca9a0e
김진희 <119389578+kyuran6@users.noreply.github.com>
2023-03-26 14:01:05 +0900

    [Feat] 메인페이지 배치, recent Image에 실시간 이미지 최신순 렌더링 (#135)
    * [Feat] 스와이퍼에 firestore에 있는 이미지 url로 불러오기 #125
    
    * [Refactor] 사용하지 않는 기능 및 주석 정리 #125
    
    * [Refactor] 주석정리 #125
    
    * [Refactor] props로 수정 #125
    
    * [Feat] 메인 페이지 배치, 이미지 렌더링 #104 (#123)
    
    * [Feat] 메인 페이지 배치완성 #104
    
    * [Refactor] 주석정리 #104
    
    * [Refactor] 주석정리 #104
    
    * [Refactor] RecentImageTitle props로 수정 #104
    
    * [Refactor] 사용하지 않는 주석정리 #104
    
    * [Feat] recent Image 최신순으로 정렬 #123
    
    * [Refactor] width height값 ImageContainer에 주기 #123
    
    * [Refactor] Proptypes으로 수정 #123

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,20 +1,74 @@
 import { useDocumentTitle } from '@/hooks/useDocumentTitle';
 import {
   MainSwiper, 
+  SeeMoreButton, 
+  SeeMoreImg,
   TopButton,
   CategoryTitle,
   Category,
+  RecentImageTitle,
 } from '@/components'
-import styled from 'styled-components/macro';
-
+import { useState, useEffect } from 'react';
+import { collection, getDocs, query, where, limit, orderBy, } from 'firebase/firestore';
+import { db } from '@/firebase/firestore';
+import * as S from './HomePage.styled';
 
 export default function HomePage() {
   useDocumentTitle('HomePage');
+  const [imgArr, setImgArr] = useState([]); 
+  const [visible, setVisible] = useState(false);
+
+  useEffect(()=>{
+  
+    const getImages = async () =>{
+      const q = query(
+        collection(db, 'images'),
+        where('user_uid',"==","EHSFq6SN4UfSAyGTw6UH"),
+        orderBy('createAt', 'desc'), //최신순으로 정렬
+        limit(6)
+      )
+      const myImgList = await getDocs(q)
+  
+      const imageList = []
+      myImgList.docs.map((doc)=>imageList.push(doc.data().url))
+     
+      imageList.sort(()=>Math.random()-0.5)
+      setImgArr(imageList);
+    }
+  
+    getImages()
+  },[])
+
+  console.log(imgArr)
+
+
 
   return <>
     <MainSwiper/>
     <CategoryTitle title={'C A T E G O R Y'} />
     <Category />
+    <RecentImageTitle title={'R E C E N T\u00A0\u00A0\u00A0 I M A G E'} />
+
+    <S.MainSection>
+    <SeeMoreImg img={imgArr[0]}/>
+    <SeeMoreImg img={imgArr[1]}/>
+    <SeeMoreImg img={imgArr[2]}/>
+    </S.MainSection>
+
+    <SeeMoreButton
+        onClick={() => {
+          setVisible(!visible);
+        }}
+        style={{marginTop: '160px', marginBottom: '90px' }}
+      >
+    </SeeMoreButton>
+
+    <S.SeeMoreSection>
+    {visible && <SeeMoreImg img={imgArr[3]}/>}
+    {visible && <SeeMoreImg img={imgArr[4]}/>}
+    {visible && <SeeMoreImg img={imgArr[5]}/>}
+    </S.SeeMoreSection>
+
     <TopButton />
     </>
 }
