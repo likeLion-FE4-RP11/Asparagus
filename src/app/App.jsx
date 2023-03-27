4ed11adfb2724d9a9adc60f904871b53ffba14f6
Seung taek Oh <elqkddl@naver.com>
2023-03-27 11:26:14 +0900

    [Refactor] Categories 동적라우팅 추가 #138

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -32,7 +32,7 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: 'Categories',
+        path: 'Categories/:name',
         element: (
           <BaseLayout>
             <CategoriesPage />
