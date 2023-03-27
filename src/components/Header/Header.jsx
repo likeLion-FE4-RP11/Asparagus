2177c8a6154a837fc52e67326fda56b8cc89e367
seovee <jinseob102@naver.com>
2023-03-27 18:07:07 +0900

    [Feat] Context API Nav제어 #148

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,6 @@ import Logo from '@/assets/Logo.svg';
 import styled from 'styled-components/macro';
 import { Link } from 'react-router-dom';
 import { getPathFromBaseUrl } from '@/utils';
-import { useAuthUser } from '@/contexts/AuthUser';
-import { useSignOut } from '@/firebase/auth';
 
 const HeaderSection = styled.section`
   display: flex;
@@ -16,17 +14,6 @@ const HeaderSection = styled.section`
 `;
 
 export function Header() {
-  const { authUser, updateAuthUser } = useAuthUser();
-
-  const { signOut } = useSignOut();
-
-  const handleSignOut = () => {
-    console.log('로그아웃');
-    signOut();
-    updateAuthUser(null);
-  };
-
-  console.log('', authUser);
   return (
     <HeaderSection>
       <h1>
@@ -34,18 +21,6 @@ export function Header() {
           <img src={Logo} alt="아이즈 갤러리 로고" />
         </Link>
       </h1>
-      {authUser ? (
-        <div>
-          <h2>인증 사용자 페이지</h2>
-          <div>
-            <ul>
-              <li>{authUser.displayName}</li>
-              <li>{authUser.email}</li>
-            </ul>
-            <button onClick={handleSignOut}>로그아웃</button>
-          </div>
-        </div>
-      ) : null}
       <Nav />
     </HeaderSection>
   );
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,8 +1,19 @@
 import Styles from './Nav.module.css';
 import { A11yHidden } from '@/components/index';
 import { NavLink } from 'react-router-dom';
+import { useAuthUser } from '@/contexts/AuthUser';
+import { useSignOut } from '@/firebase/auth';
 
 export function Nav() {
+  const { authUser, updateAuthUser } = useAuthUser();
+  const { signOut } = useSignOut();
+
+  const handleSignOut = () => {
+    console.log('로그아웃');
+    signOut();
+    updateAuthUser(null);
+  };
+
   return (
     <nav className={Styles.Nav}>
       <A11yHidden as="h2">메인 메뉴</A11yHidden>
@@ -10,12 +21,29 @@ export function Nav() {
         <li>
           <NavLink to="/upload">Upload</NavLink>
         </li>
-        <li>
-          <NavLink to="/signin">Log In</NavLink>
-        </li>
-        <li>
-          <NavLink to="/signup">Sign Up</NavLink>
-        </li>
+        {authUser ? (
+          <>
+            <li>
+              <NavLink to="/">
+                <button className={Styles.Navtest} onClick={handleSignOut}>
+                  Logout
+                </button>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/">{authUser.name}님</NavLink>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <NavLink to="/signin">Login</NavLink>
+            </li>
+            <li>
+              <NavLink to="/signup">Sign Up</NavLink>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
