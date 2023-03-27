9425cf4aac1e8de987f2a9376c5486ecfa01f587
김진희 <119389578+kyuran6@users.noreply.github.com>
2023-03-28 01:27:49 +0900

    [Refactor] Seemore Image에 실시간 이미지 렌더링 (#157)
    * [Feat] 스와이퍼에 firestore에 있는 이미지 url로 불러오기 #125
    
    * [Refactor] 사용하지 않는 기능 및 주석 정리 #125
    
    * [Refactor] 주석정리 #125
    
    * [Refactor] props로 수정 #125
    
    * [Feat] 메인 페이지 배치, 이미지 렌더링 #104 (#123)
    
    * [Feat] 메인 페이지 배치완성 #104
    
    * [Refactor] 주석정리 #104
    
    * [Refactor] 주석정리 #104
    
    * [Refactor] RecentImageTitle props로 수정 #104
    
    * [Refactor] 사용하지 않는 주석정리 #104
    
    * [Feat] recent Image 최신순으로 정렬 #123
    
    * [Refactor] ImageContainer 수정 #134
    
    * [Refactor] Hompage 수정 #134
    
    * [Refactor] 주석제거 #134
    
    * [Refactor] 중복제거 #134

--- src/components/SeeMoreImage/SeeMoreImg.jsx
+++ src/components/SeeMoreImage/SeeMoreImg.jsx
@@ -1,28 +1,10 @@
 import styled from 'styled-components/macro';
 import { getColor } from '@/theme/utils';
-import { BaseImage } from '@/components';
+import { ImageContainer } from '@/components';
 import PropTypes from 'prop-types';
 
-
-const ImageContainer = styled.div`
-  margin-top: 20px;
-  padding: 20px;
-  background-color: ${(props) => props.bgColor};
-  text-align: left;
-  z-index: 1;
-  width: 29.4375rem;
-  height: 37.5rem;
-  margin-right: 71px;
-`;
-
 export function SeeMoreImg({ src }) {
-
- 
-  return (
-    <ImageContainer width="29.4375rem" height="37.5rem">
-        <BaseImage img={src}/>
-    </ImageContainer>
-  );
+  return <ImageContainer width="29.4375rem" height="37.5rem" src={src} />;
 }
 
 ImageContainer.displayName = 'ImageContainer';
