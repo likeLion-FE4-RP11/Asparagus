44f7cc6b35bf5b5bde29941ccff694cfc7efc2d6
문나래 <skfo789@naver.com>
2023-03-28 02:07:48 +0900

    [Refactor]  테마아이콘 파일 교체 및 카테고리에 스와이퍼 삽입

--- src/pages/Categories/CategoriesPage.jsx
+++ src/pages/Categories/CategoriesPage.jsx
@@ -1,5 +1,6 @@
+import * as S from './CategoriesPage.styled';
 import { useDocumentTitle } from '@/hooks/useDocumentTitle';
-import pencilImg from '@/assets/pencil-icon.svg';
+import { getColor } from '@/theme/utils';
 import { useState, useEffect, useLayoutEffect } from 'react';
 import {
   collection,
@@ -8,7 +9,6 @@ import {
   limit,
   onSnapshot,
 } from 'firebase/firestore';
-
 import { db } from '@/firebase/firestore';
 import {
   ToggleButton,
@@ -17,12 +17,14 @@ import {
   ImageContainer,
   UseHover,
   TopButton,
+  MainSwiper,
 } from '@/components';
 
-import * as S from './CategoriesPage.styled';
-import { getColor } from '@/theme/utils';
 import { LightTheme, DarkTheme, GlobalStyles } from '@/theme/theme';
-import { ThemeProvider } from 'styled-components';
+import { ThemeContext, ThemeProvider } from 'styled-components';
+import pencilImg from '@/assets/pencil-icon.svg';
+import Dark from '@/assets/Dark.svg';
+import Light from '@/assets/Light.svg';
 
 export default function CategoriesPage() {
   useDocumentTitle('Categories');
@@ -55,7 +57,6 @@ export default function CategoriesPage() {
     });
 
     return () => {
-      console.log('onSnapshot 이벤트 구독 해지');
       unsubscribe();
     };
   }, []);
@@ -75,11 +76,8 @@ export default function CategoriesPage() {
     setImgIdArr(idList);
   }, [imageDataArr]);
 
-  console.log(imgArr);
-  console.log(imgIdArr);
-
   // 텍스트 편집 기능
-  const [text, setText] = useState('I Love Traveled here with my friends!');
+  const [text, setText] = useState('I traveled here with my friends!');
   const [isEditable, setIsEditable] = useState(false);
 
   const handleDoubleClick = () => {
@@ -96,7 +94,7 @@ export default function CategoriesPage() {
     }
   };
 
-  // 테마변경
+  // context api 테마 변경
   const [theme, setTheme] = useState('light');
 
   const setMode = (mode) => {
@@ -114,134 +112,106 @@ export default function CategoriesPage() {
   }, []);
 
   return (
-    <ThemeProvider theme={theme === 'light' ? LightTheme : DarkTheme}>
-      <GlobalStyles />
-      <ToggleButton />
-      <S.Themebutton onClick={() => themeTogggler()}>
-        {theme === 'dark' ? (
-          <svg
-            width="50"
-            height="54"
-            viewBox="0 0 50 54"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1 26.9815C1 12.9888 11.9894 1.38308 26.0416 1.00932C17.31 18.5251 28.9568 39.5412 48.2791 41.33C43.6542 48.368 35.779 53 26.7953 53C12.5644 53 1 41.3513 1 26.9815Z"
-              stroke="white"
-              stroke-width="2"
-            />
-          </svg>
-        ) : (
-          <svg
-            width="60"
-            height="60"
-            viewBox="0 0 60 60"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M50.1595 37.7192L49.7202 38.1585V38.7798V49.7202H38.7798H38.1585L37.7192 50.1595L30 57.8787L22.2808 50.1595L21.8415 49.7202H21.2202H10.2798V38.7798V38.1585L9.8405 37.7192L2.12132 30L9.8405 22.2808L10.2798 21.8415V21.2202V10.2798H21.2202H21.8415L22.2808 9.8405L30 2.12132L37.7192 9.8405L38.1585 10.2798H38.7798H49.7202V21.2202V21.8415L50.1595 22.2808L57.8787 30L50.1595 37.7192ZM12.5849 30C12.5849 39.6083 20.3917 47.4151 30 47.4151C39.6083 47.4151 47.4151 39.6083 47.4151 30C47.4151 20.3917 39.6083 12.5849 30 12.5849C20.3917 12.5849 12.5849 20.3917 12.5849 30Z"
-              stroke="black"
-              stroke-opacity="0.51"
-              stroke-width="3"
-            />
-          </svg>
-        )}
-      </S.Themebutton>
-      <ImageContainer
-        width={'1557px'}
-        height={'769px'}
-        style={{ position: 'relative' }}
-        src={imgArr[0]}
-      ></ImageContainer>
-      <S.TextContainer>
-        <S.ImageTitle color={getColor('white')}>Travel</S.ImageTitle>
-        <S.ImageLogo
-          src={pencilImg}
-          onDoubleClick={handleDoubleClick}
-          alt="대표 사진 내용 편집하기"
-        />
-        {isEditable ? (
-          <input
-            type="text"
-            value={text}
-            onChange={handleChange}
-            onKeyDown={handleKeyDown}
+    <ThemeContext.Provider>
+      <ThemeProvider theme={theme === 'light' ? LightTheme : DarkTheme}>
+        <GlobalStyles />
+        <ToggleButton />
+        <S.Themebutton onClick={() => themeTogggler()}>
+          {theme === 'dark' ? (
+            <img src={Dark} alt="다크모드 활성화" />
+          ) : (
+            <img src={Light} alt="라이트모드 활성화" />
+          )}
+        </S.Themebutton>
+        <MainSwiper />
+        <S.TextContainer>
+          <S.ImageTitle color={getColor('white')}>Travel</S.ImageTitle>
+          <S.ImageLogo
+            src={pencilImg}
+            onDoubleClick={handleDoubleClick}
+            alt="대표 사진 내용 편집하기"
           />
-        ) : (
-          <S.BaseText color={getColor('white')}>{text}</S.BaseText>
-        )}
-        <LikeButton />
-      </S.TextContainer>
-      <S.FirstContainer>
-        <UseHover
-          width={'894'}
-          height={'525'}
-          description={descriptionArr[1]}
-          datasetKey={imgIdArr[1]}
-          src={imgArr[1]}
-        />
-        <UseHover
-          width={'632'}
-          height={'525'}
-          description={descriptionArr[2]}
-          datasetKey={imgIdArr[2]}
-          src={imgArr[2]}
-        />
-      </S.FirstContainer>
-      <S.SecondContainer>
-        <UseHover
-          width={'626'}
-          height={'525'}
-          description={descriptionArr[3]}
-          datasetKey={imgIdArr[3]}
-          src={imgArr[3]}
-        />
-        <UseHover
-          width={'900'}
-          height={'525'}
-          description={descriptionArr[4]}
-          datasetKey={imgIdArr[4]}
-          src={imgArr[4]}
-        />
-      </S.SecondContainer>
-      <DesignParagraph>
-        #Snapshot #Golden Bridge #Photographer #America #I want freedom
-      </DesignParagraph>
-      <S.ThirdContainet>
-        <UseHover
-          width={'1097'}
-          height={'633'}
-          description={descriptionArr[5]}
-          datasetKey={imgIdArr[5]}
-          src={imgArr[5]}
-        />
-        <UseHover
-          width={'430'}
-          height={'633'}
-          description={descriptionArr[6]}
-          datasetKey={imgIdArr[6]}
-          src={imgArr[6]}
-        />
-      </S.ThirdContainet>
-      <S.FourthContainet>
-        <UseHover
-          width={'739'}
-          height={'416'}
-          description={descriptionArr[7]}
-          datasetKey={imgIdArr[7]}
-          src={imgArr[7]}
-        />
-        <UseHover
-          width={'789'}
-          height={'416'}
-          description={descriptionArr[8]}
-          datasetKey={imgIdArr[8]}
-          src={imgArr[8]}
-        />
-      </S.FourthContainet>
-      <TopButton />
-    </ThemeProvider>
+          {isEditable ? (
+            <input
+              type="text"
+              value={text}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+            />
+          ) : (
+            <S.BaseText color={getColor('white')}>{text}</S.BaseText>
+          )}
+          <LikeButton />
+        </S.TextContainer>
+        <S.FirstContainer>
+          <UseHover
+            width={'894'}
+            height={'525'}
+            description={descriptionArr[1]}
+            datasetKey={imgIdArr[1]}
+            src={imgArr[1]}
+          />
+          <UseHover
+            width={'632'}
+            height={'525'}
+            description={descriptionArr[2]}
+            datasetKey={imgIdArr[2]}
+            src={imgArr[2]}
+          />
+        </S.FirstContainer>
+        <S.SecondContainer>
+          <UseHover
+            width={'626'}
+            height={'525'}
+            description={descriptionArr[3]}
+            datasetKey={imgIdArr[3]}
+            src={imgArr[3]}
+          />
+          <UseHover
+            width={'900'}
+            height={'525'}
+            description={descriptionArr[4]}
+            datasetKey={imgIdArr[4]}
+            src={imgArr[4]}
+          />
+        </S.SecondContainer>
+        <DesignParagraph>
+          #Snapshot #Camera #Photographer #America #I want freedom
+        </DesignParagraph>
+        <S.ThirdContainet>
+          <UseHover
+            width={'1097'}
+            height={'633'}
+            description={descriptionArr[5]}
+            datasetKey={imgIdArr[5]}
+            src={imgArr[5]}
+          />
+          <UseHover
+            width={'430'}
+            height={'633'}
+            description={descriptionArr[6]}
+            datasetKey={imgIdArr[6]}
+            src={imgArr[6]}
+          />
+        </S.ThirdContainet>
+        <S.FourthContainet>
+          <UseHover
+            width={'739'}
+            height={'416'}
+            description={descriptionArr[7]}
+            datasetKey={imgIdArr[7]}
+            src={imgArr[7]}
+          />
+          <UseHover
+            width={'789'}
+            height={'416'}
+            description={descriptionArr[8]}
+            datasetKey={imgIdArr[8]}
+            src={imgArr[8]}
+          />
+        </S.FourthContainet>
+        <TopButton />
+      </ThemeProvider>
+    </ThemeContext.Provider>
   );
 }
