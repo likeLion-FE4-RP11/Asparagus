29b91695ba25ce281666be4b91197e3432c8fabc
Yesong Choi <to06109@naver.com>
2023-03-29 10:26:04 +0900

    [Fix] 헤더 로고 클릭 시 페이지 이동 안되는 문제 해결

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import { Nav } from '@/components/index';
 import styled from 'styled-components/macro';
 import { Link } from 'react-router-dom';
-import { getPathFromBaseUrl } from '@/utils';
 import Logo from '@/assets/Logo.svg';
 
 const HeaderSection = styled.section`
@@ -17,7 +16,7 @@ export function Header() {
   return (
     <HeaderSection className="header">
       <h1>
-        <Link to={getPathFromBaseUrl()}>
+        <Link to={'/'}>
           <img src={Logo} alt="아이즈 갤러리 로고" />
         </Link>
       </h1>
