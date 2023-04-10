6417c8515cb5d0580025afe9f73dc6d55d1eda8b
seovee <jinseob102@naver.com>
2023-04-10 14:19:21 +0900

    [Style] 피드백 주석 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,12 +15,15 @@ const HeaderSection = styled.section`
 
 export function Header() {
   const theme = useTheme();
-
+  // 지금은 페이지에서만 관리되고 있음. 전역상태관리가 필요.
+  // 1. 사이드이펙트 처리 -> useEffect를 사용해서 테마정보를 가져와야 한다.
+  // 2. 테마를 글로벌 스테이트로 옮기는 것
+  console.log(theme);
   return (
     <HeaderSection className="header">
       <h1>
         <Link to={'/'}>
-          <LogoIcon fill={'black'} />
+          <LogoIcon fill={'white'} />
         </Link>
       </h1>
       <Nav />
